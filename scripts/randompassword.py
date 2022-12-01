69eada94a3c98f3dc4a50ba8a078172417ed78a5
Pessi Vilppolahti <pessiv@outlook.com>
2022-12-01 19:08:25 +0000

    finished a thing

--- scripts/randompassword.py
+++ scripts/randompassword.py
@@ -5,7 +5,14 @@ import string
 def main():
 	if len(sys.argv) > 0:
 		if sys.argv[1].isnumeric():
-			to_print = ''.join((random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for codenum in range(int(sys.argv[1]))))
+			if int(sys.argv[1]) <= 0:
+				to_print = ''.join((random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for codenum in range(8)))
+				print(to_print)
+			else:
+				to_print = ''.join((random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for codenum in range(int(sys.argv[1]))))
+				print(to_print)
+		else:
+			to_print = ''.join((random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for codenum in range(8)))
 			print(to_print)
 if __name__ == "__main__":
 	main()
